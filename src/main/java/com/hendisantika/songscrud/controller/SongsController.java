fe5662c8fca365d8bccb48575d08a5c2236966e6
Hendi Santika <hendisantika@gmail.com>
2022-04-02 08:47:26 +0700

    #8 - Add getAll API in SongsController

--- src/main/java/com/hendisantika/songscrud/controller/SongsController.java
+++ src/main/java/com/hendisantika/songscrud/controller/SongsController.java
@@ -2,6 +2,9 @@ package com.hendisantika.songscrud.controller;
 
 import com.hendisantika.songscrud.repository.SongsRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,4 +23,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class SongsController {
 
     private SongsRepository songsRepository;
+
+    @GetMapping
+    public ResponseEntity<?> getAll() {
+        return new ResponseEntity<>(songsRepository.getAll(), HttpStatus.OK);
+    }
 }
