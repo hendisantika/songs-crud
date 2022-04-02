0ed566321611acda9ac89034d77d97b173ba486b
Hendi Santika <hendisantika@gmail.com>
2022-04-02 08:47:38 +0700

    #9 - Add getAllByYear API in SongsController

--- src/main/java/com/hendisantika/songscrud/controller/SongsController.java
+++ src/main/java/com/hendisantika/songscrud/controller/SongsController.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,5 +28,10 @@ public class SongsController {
     @GetMapping
     public ResponseEntity<?> getAll() {
         return new ResponseEntity<>(songsRepository.getAll(), HttpStatus.OK);
+    }
+
+    @GetMapping("/year/{year}")
+    public ResponseEntity<?> getAllByYear(@PathVariable("year") Integer year) {
+        return new ResponseEntity<>(songsRepository.getAllByYear(year), HttpStatus.OK);
     }
 }
