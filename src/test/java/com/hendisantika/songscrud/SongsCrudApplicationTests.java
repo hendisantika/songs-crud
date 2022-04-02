edbe95100cfeaa72be39e73ff23cf5746e177f1f
Hendi Santika <hendisantika@gmail.com>
2022-04-02 08:42:20 +0700

    #1 - Initial Commit

---
+++ src/test/java/com/hendisantika/songscrud/SongsCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika.songscrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SongsCrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
